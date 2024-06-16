1243f980d4b7580f75335aa81bd8c3b68caa3a0c
Hafizal <90334700+muhammadhafizal16@users.noreply.github.com>
2024-06-16 00:08:33 -0700

    Website Project
    Toko Gadget Galaxy Menggunakan Bahasa PHP

---
+++ pelanggan/about.php
@@ -0,0 +1,52 @@
+<?php
+ 
+  include "header.php";
+  
+?>
+
+<!-- Header -->
+
+<!-- HERO SECTION-->
+<section class="py-5 bg-light">
+  <div class="container">
+    <div class="row px-4 px-lg-5 py-lg-4 align-items-center">
+      <div class="col-lg-6">
+        <h1 class="h2 text-uppercase mb-0">About</h1>
+      </div>
+      <div class="col-lg-6 text-lg-right">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb justify-content-lg-end mb-0 px-0">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">About</li>
+            </ol>
+          </nav>
+      </div>
+    </div>
+    </div>
+</section>
+
+<section class="py-5">
+    <div class="container">
+  <div class="card mb-3">
+  <div class="row no-gutters">
+   
+    <div class="col-md-12">
+      <div class="card-body shadow bg-white rounded">
+        <h2 class="card-title text-center">About Us</h2>
+        <hr>  
+        <p class="card-text">Toko Gadget Galaxy adalah toko ritel atau online yang khusus menjual berbagai produk teknologi. Produk yang tersedia di toko ini mencakup smartphone, tablet, smartwatch, dan aksesori yang semuanya berada dalam ekosistem Galaxy. Smartphone yang ditawarkan termasuk Iphone dari seri bawah sampai dengan seri atas. Toko Gadget Galaxy Aksesori juga menjual seperti casing, pengisi daya, dan pelindung layar juga bisa ditemukan di toko ini. Toko Gadget Galaxy sering menawarkan promosi dan bundling menarik, serta memberikan layanan purna jual seperti garansi dan dukungan teknis, memastikan pengalaman belanja yang memuaskan bagi pelanggan.</p>
+
+       
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</section>
+<!-- footer -->
+<?php 
+  include "footer.php";
+?>
